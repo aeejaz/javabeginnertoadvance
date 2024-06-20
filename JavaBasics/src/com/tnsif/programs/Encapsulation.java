60a0d43c3a849b747151f34130174dd983664503
Khan Ejaj Ahmed <aejazkhan414@gmail.com>
2024-06-20 10:46:52 +0530

    intro to encapsulation and java basics

---
+++ JavaBasics/src/com/tnsif/programs/Encapsulation.java
@@ -0,0 +1,12 @@
+// Program to demonstrate encapsulation
+package com.tnsif.programs;
+
+public class Encapsulation {
+    int serialnum; // Data members
+    String name;
+    int age;
+
+    public void show() { // Member function
+        System.out.println("serialnubetr" + serialnum + "\t Name:" + name + "\t Age:" + age);
+    }
+}
---
+++ JavaBasics/src/com/tnsif/programs/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.programs;
+
+public class EncapsulationDemo {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Encapsulation obj = new Encapsulation(); // Created an object
+        // classname obj_name = new ClassName();
+        obj.serialnum = 16; // Calling data member by using obj
+        obj.name = "aejaz";
+        obj.age = 21;
+        obj.show();
+    }
+}
